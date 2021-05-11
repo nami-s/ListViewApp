583e5eb7ff9041ea785867ea7da05331f3b0dd5f
nami-s <na.s1222.10feettt@gmail.com>
2021-05-11 17:41:15 +0900

    [add]データを準備してListViewに表示記述例

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = (ListView) findViewById(R.id.listView);
+
+        // データを準備
+        ArrayList<String> items = new ArrayList<>();
+        for (int i = 0; i < 30; i++){
+            items.add("item-" + i);
+        }
+
+        //　Adapter - ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.list_item,
+                items
+        );
+
+        //ListViewに表示
+        myListView.setAdapter(adapter);
     }
 }
