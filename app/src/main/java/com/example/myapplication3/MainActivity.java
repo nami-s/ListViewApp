16d64e2d9801a6ef205f4b993d6e290249da548d
nami-s <na.s1222.10feettt@gmail.com>
2021-05-14 17:55:55 +0900

    [add]Userクラス

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -20,8 +21,39 @@ public class MainActivity extends AppCompatActivity {
 
         // データを準備
 
+
         //　Adapter - ArrayAdapter
 
         //ListViewに表示
+    }
+
+    public class User{
+        private Bitmap icon;
+        private String name;
+        private String loc;
+
+        public String getLoc() {
+            return loc;
+        }
+
+        public void setLoc(String loc) {
+            this.loc = loc;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Bitmap getIcon() {
+            return icon;
+        }
+
+        public void setIcon(Bitmap icon) {
+            this.icon = icon;
+        }
     }
 }
